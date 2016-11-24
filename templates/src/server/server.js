5327fa8ee3cd7631b50c67442ad539aecac33a2f
Ras <contact@rasputin.io>
2016-11-23 17:07:50 -0800

    Add server script

---
+++ templates/src/server/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import logger from './logger.js'
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(logger.middleware)
+
+app.get('/', (req,res) => {
+	res.send('Hello World!')
+})
+
+app.listen(port, () => {
+	logger.info('Server listening on port %s', port)
+})
