761591026620c4047f88425a61dc59f71158b805
Ras <contact@rasputin.io>
2016-11-30 20:25:31 -0800

    Configure sessions

--- templates/src/server/server.js
+++ templates/src/server/server.js
@@ -1,15 +1,25 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import sessions from 'client-sessions'
 import logger from './logger.js'
 import _db from './config/db.js'
 import auth from './routes/auth.js'
 const app = express()
 const port = process.env.PORT || 5000
 
+app.set('trust proxy', true)
+
 app.use(logger.middleware)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
 	extended: false
+}))
+app.use(sessions({
+	cookieName: 'session',
+	secret: process.env.SESS_SECRET || 'You forgot to set a session secret, dingle!!!1!',
+	duration: 24 * 60 * 60 * 1000,
+	activeDuration: 5 * 60 * 1000,
+	secureProxy: true
 }))
 app.use('/auth', auth)
 
